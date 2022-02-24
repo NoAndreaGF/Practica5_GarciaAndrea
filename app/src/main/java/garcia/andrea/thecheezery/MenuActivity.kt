d07ed3f459d15dc31b960ae1e729395a6b68ac0c
AndreaGarcía <andrea.garcia216626@potros.itson.edu.mx>
2022-02-24 10:48:49 -0700

    Commit 2

--- app/src/main/java/garcia/andrea/thecheezery/MenuActivity.kt
+++ app/src/main/java/garcia/andrea/thecheezery/MenuActivity.kt
@@ -1,11 +1,43 @@
 package garcia.andrea.thecheezery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        var btnCold: Button = findViewById(R.id.button_cold_drinks)
+        var btnHot: Button = findViewById(R.id.button_hot_drinks)
+        var btnSweets: Button = findViewById(R.id.button_sweets)
+        var btnSalties: Button = findViewById(R.id.button_salties)
+
+        btnCold.setOnClickListener{
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            intent.putExtra("menuType", "coldDrinks")
+            startActivity(intent)
+        }
+
+        btnHot.setOnClickListener{
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            intent.putExtra("menuType", "hotDrinks")
+            startActivity(intent)
+        }
+        btnSweets.setOnClickListener{
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            intent.putExtra("menuType", "sweets")
+            startActivity(intent)
+        }
+        btnSalties.setOnClickListener{
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            intent.putExtra("menuType", "salties")
+            startActivity(intent)
+        }
     }
 }
